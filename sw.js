2c5b3c0c0f8b5206e4b05e3499fd77dba4e7e7d4
w256s5kkjq-maker <w256s5kkjq@privaterelay.appleid.com>
2026-09-25 14:35:22 +0700

    Add offline service worker

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE="iphone-upscaler-v1";
+const ASSETS=["./","./index.html","./manifest.webmanifest"];
+self.addEventListener("install",event=>{
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
+});
+self.addEventListener("activate",event=>{
+  event.waitUntil(Promise.all([
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),
+    self.clients.claim()
+  ]));
+});
+self.addEventListener("fetch",event=>{
+  if(event.request.method!=="GET") return;
+  event.respondWith(
+    caches.match(event.request).then(cached=>{
+      if(cached) return cached;
+      return fetch(event.request).then(response=>{
+        const copy=response.clone();
+        caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+        return response;
+      }).catch(()=>caches.match("./index.html"));
+    })
+  );
+});
